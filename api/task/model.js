b4b3ed538d5885c0b17c5ebacc87baa8a9c7fb25
luisbryandr <luisbdominguezr@gmail.com>
2022-01-21 18:56:50 -0500

    task wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,24 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const getAll = async () => {
+    const tasksDraft = await db('tasks')
+    const tasks = tasksDraft.map(task => {
+        if(task.task_completed === 1 ) {
+            return {
+                ...task,
+                task_completed: true,
+            }
+        }else {
+            return {
+                ...task,
+                task_completed: false,
+            }
+        }
+    })
+    return tasks
+}
+
+module.exports = {
+    getAll
+}
--- api/task/router.js
+++ api/task/router.js
@@ -3,8 +3,15 @@ const express = require('express')
 
 const router = express.Router()
 
+const Task = require('./model')
+const md = require('./middleware')
+
+
 router.get('/', (req, res, next) => {
-    console.log('get tasks router wired')
+    Task.getAll()
+        .then(task => {
+            res.json(task)
+        })
 })
 
 router.post('/', (req, res, next) => {

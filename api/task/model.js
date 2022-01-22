a813607d8dcaa959f44a8cf04670a53d7472eff5
luisbryandr <luisbdominguezr@gmail.com>
2022-01-21 19:38:47 -0500

    get tasks endpoint working

--- api/task/model.js
+++ api/task/model.js
@@ -2,7 +2,13 @@
 const db = require('../../data/dbConfig')
 
 const getAll = async () => {
-    const tasksDraft = await db('tasks')
+    const tasksDraft = await db('tasks').join('projects','tasks.project_id','=','projects.project_id')
+    .select("tasks.tasks_id",
+    "tasks.task_description",
+    "tasks.task_notes",
+    "tasks.task_completed",
+    "projects.project_name",
+    "projects.project_description")
     const tasks = tasksDraft.map(task => {
         if(task.task_completed === 1 ) {
             return {
